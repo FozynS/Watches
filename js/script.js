4919a96c5a54928cf39f4e9ef80a6c255f474a40
Danylo <wolf44488@gmail.com>
2023-08-23 18:00:09 +0200

    Add site animation

--- js/script.js
+++ js/script.js
@@ -1,46 +1,71 @@
 // //!Site Animation */
-// document.addEventListener("DOMContentLoaded", function () {
-//     const header = document.querySelector(".header");
-//     const main = document.querySelector(".main");
-//     const mainSlideImages = document.querySelectorAll(".main__slide img");
-//     const titles = document.querySelectorAll(".title");
-//     const descriptions = document.querySelectorAll(".description");
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector(".header");
+    const main = document.querySelector(".main");
+    const mainSlideImages = document.querySelectorAll(".main__slide img");
+    const titles = document.querySelectorAll(".title");
+    const descriptions = document.querySelectorAll(".description");
 
-//     // Спрятать элементы
-//     header.style.opacity = "0";
-//     main.style.opacity = "0";
-//     mainSlideImages.forEach(image => image.style.opacity = "0");
-//     titles.forEach(title => title.style.opacity = "0");
-//     descriptions.forEach(description => description.style.opacity = "0");
+    // Спрятать элементы
+    header.style.opacity = "0";
+    main.style.opacity = "0";
+    mainSlideImages.forEach(image => image.style.opacity = "0");
+    titles.forEach(title => title.style.opacity = "0");
+    descriptions.forEach(description => description.style.opacity = "0");
 
-//     // Функция для плавного появления элемента
-//     function fadeIn(element, delay) {
-//         setTimeout(function () {
-//             element.style.transition = "opacity 1s ease-out";
-//             element.style.opacity = "1";
-//         }, delay);
-//     }
+    // Функция для плавного появления элемента
+    function fadeIn(element, delay) {
+        setTimeout(function () {
+            element.style.transition = "opacity 1s ease-out";
+            element.style.opacity = "1";
+        }, delay);
+    }
 
-//     // Плавное появление изображений слайдов поочередно
-//     mainSlideImages.forEach((image, index) => {
-//         fadeIn(image, 300 * index); // Задержка увеличена для последовательного появления
-//     });
+    // Плавное появление изображений слайдов поочередно
+    mainSlideImages.forEach((image, index) => {
+        fadeIn(image, 300 * index); // Задержка увеличена для последовательного появления
+    });
 
-//     // Плавное появление заголовков после появления изображений
-//     titles.forEach((title, index) => {
-//         fadeIn(title, 300 * mainSlideImages.length + 300 * index); // Задержка после изображений
-//     });
+    // Плавное появление заголовков после появления изображений
+    titles.forEach((title, index) => {
+        fadeIn(title, 300 * mainSlideImages.length + 300 * index); // Задержка после изображений
+    });
 
-//     // Плавное появление описаний после появления заголовков
-//     descriptions.forEach((description, index) => {
-//         fadeIn(description, 300 * (mainSlideImages.length + titles.length) + 300 * index); // Задержка после заголовков
-//     });
+    // Плавное появление описаний после появления заголовков
+    descriptions.forEach((description, index) => {
+        fadeIn(description, 300 * (mainSlideImages.length + titles.length) + 300 * index); // Задержка после заголовков
+    });
 
-//     // Плавное появление header
-//     fadeIn(header, 1000); // Задержка после описаний
+    // Плавное появление header
+    fadeIn(header, 1000); // Задержка после описаний
 
-//     // Плавное появление main
-//     fadeIn(main, 1200); // Задержка после header
-//      // Обработка события прокрутки
-// });
+    // Плавное появление main
+    fadeIn(main, 1200); // Задержка после header
+     // Обработка события прокрутки
+});
 
+//*!Scroll Animation */
+const main = document.querySelector('.main'),
+        slideWrapper = document.querySelector('.main__slider_container-wrapper'),
+        slide = document.querySelectorAll('.main__slide'),
+        slideImage = document.querySelector('.main__slide-img'),
+        slideTitle = document.querySelector('title'),
+        slideDescription = document.querySelector('.description'),
+        width = window.getComputedStyle(slideWrapper).width;
+
+    let slideIndex = 0;
+    let offset = 0;
+    
+
+// slide.forEach(slide => {
+//     slide.style.width = width;
+// }); 
+
+    console.log(slide.length)
+
+main.addEventListener('scroll', () => {
+    if(scroll){
+        // slideWrapper.style.transform = 'translateY(-1100px)'
+    }
+
+});
