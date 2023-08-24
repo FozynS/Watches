f1395db5b31a2148b1990969e25674071d15fe78
Danylo <wolf44488@gmail.com>
2023-08-24 23:25:05 +0200

    Completed the work with JS

--- js/script.js
+++ js/script.js
@@ -1,71 +1,107 @@
-// //!Site Animation */
+//!Site Animation */
 document.addEventListener("DOMContentLoaded", function () {
-    const header = document.querySelector(".header");
-    const main = document.querySelector(".main");
-    const mainSlideImages = document.querySelectorAll(".main__slide img");
-    const titles = document.querySelectorAll(".title");
-    const descriptions = document.querySelectorAll(".description");
+    const header = document.querySelector(".header"),
+            main = document.querySelector(".main"),
+            mainSlideImages = document.querySelectorAll(".main__slide img"),
+            titles = document.querySelectorAll(".title"),
+            descriptions = document.querySelectorAll(".description");
 
-    // Спрятать элементы
-    header.style.opacity = "0";
-    main.style.opacity = "0";
-    mainSlideImages.forEach(image => image.style.opacity = "0");
-    titles.forEach(title => title.style.opacity = "0");
-    descriptions.forEach(description => description.style.opacity = "0");
+    header.classList.add("hide");
+    main.classList.add("hide");
+    mainSlideImages.forEach(image => image.classList.add("hide"));
+    titles.forEach(title => title.classList.add("hide"));
+    descriptions.forEach(description => description.classList.add("hide"));
 
-    // Функция для плавного появления элемента
     function fadeIn(element, delay) {
         setTimeout(function () {
             element.style.transition = "opacity 1s ease-out";
-            element.style.opacity = "1";
+            element.classList.remove("hide");
+            element.classList.add("show");
         }, delay);
     }
 
-    // Плавное появление изображений слайдов поочередно
     mainSlideImages.forEach((image, index) => {
-        fadeIn(image, 300 * index); // Задержка увеличена для последовательного появления
+        fadeIn(image, 300 * index); 
     });
 
-    // Плавное появление заголовков после появления изображений
     titles.forEach((title, index) => {
-        fadeIn(title, 300 * mainSlideImages.length + 300 * index); // Задержка после изображений
+        fadeIn(title, 300 * mainSlideImages.length + 300 * index); 
     });
 
-    // Плавное появление описаний после появления заголовков
     descriptions.forEach((description, index) => {
-        fadeIn(description, 300 * (mainSlideImages.length + titles.length) + 300 * index); // Задержка после заголовков
+        fadeIn(description, 300 * (mainSlideImages.length + titles.length) + 300 * index); 
     });
 
-    // Плавное появление header
-    fadeIn(header, 1000); // Задержка после описаний
+    fadeIn(header, 1000); 
 
-    // Плавное появление main
-    fadeIn(main, 1200); // Задержка после header
-     // Обработка события прокрутки
+    fadeIn(main, 1200); 
 });
 
 //*!Scroll Animation */
 const main = document.querySelector('.main'),
         slideWrapper = document.querySelector('.main__slider_container-wrapper'),
-        slide = document.querySelectorAll('.main__slide'),
-        slideImage = document.querySelector('.main__slide-img'),
-        slideTitle = document.querySelector('title'),
-        slideDescription = document.querySelector('.description'),
-        width = window.getComputedStyle(slideWrapper).width;
+        slides = document.querySelectorAll('.main__slide'),
+        slideImage = document.querySelectorAll('.main__slide-img'),
+        slideTitleCompany = document.querySelectorAll('.title__company'),
+        slideTitleModal = document.querySelectorAll('.title__modal'),
+        slideDescription = document.querySelectorAll('.description'),
+        clickMe = document.querySelector('.header-clickMe');
 
-    let slideIndex = 0;
-    let offset = 0;
+let currentSlideIndex = 0;
+
+function changeSlide(index) {
+    currentSlideIndex = index;
+    const translateY = -currentSlideIndex * 100;
+
+    slideWrapper.style.transform = `translateY(${translateY}%)`;
+    slideWrapper.style.transition = 'all 1.3s ease-in-out';
+}
+
+clickMe.addEventListener('click', () => {
+    animateHideBlocks();
     
+    setTimeout(() => {
+        const nextSlideIndex = (currentSlideIndex + 1) % slides.length;
+        changeSlide(nextSlideIndex);
+        animateShowBlocks();
 
-// slide.forEach(slide => {
-//     slide.style.width = width;
-// }); 
-
-    console.log(slide.length)
-
-main.addEventListener('scroll', () => {
-    if(scroll){
-        // slideWrapper.style.transform = 'translateY(-1100px)'
-    }
-
+        if (nextSlideIndex === 3) {
+            setTimeout(() => {
+                changeSlide(0);
+            }, 1700); 
+        }
+    }, 1700); 
 });
+
+function animateHideBlocks() {
+    setTimeout(() => {
+        slides[currentSlideIndex].classList.remove('show');
+        slides[currentSlideIndex].classList.add('hide');
+    }, 1700);
+    slideImage[currentSlideIndex].classList.add('hide-up');
+    slideTitleCompany[currentSlideIndex].classList.add('hide-left');
+    slideTitleModal[currentSlideIndex].classList.add('hide-right');
+    slideDescription[currentSlideIndex].classList.add('hide-down');
+
+    slideImage[currentSlideIndex].classList.remove('show-down');
+    slideTitleCompany[currentSlideIndex].classList.remove('show-right');
+    slideTitleModal[currentSlideIndex].classList.remove('show-left');
+    slideDescription[currentSlideIndex].classList.remove('show-down');
+}
+
+function animateShowBlocks() {
+    setTimeout(() => {
+        slides[currentSlideIndex].classList.remove('hide');
+        slides[currentSlideIndex].classList.add('show');
+    }, 1700);
+    slideImage[currentSlideIndex].classList.add('show-down');
+    slideTitleCompany[currentSlideIndex].classList.add('show-right');
+    slideTitleModal[currentSlideIndex].classList.add('show-left');
+    slideDescription[currentSlideIndex].classList.add('show-down');
+
+    slideImage[currentSlideIndex].classList.remove('hide-up');
+    slideTitleCompany[currentSlideIndex].classList.remove('hide-left');
+    slideTitleModal[currentSlideIndex].classList.remove('hide-right');
+    slideDescription[currentSlideIndex].classList.remove('hide-down');
+
+}
